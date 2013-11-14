a88545050783c848da3e6564a74c4e9fcfbee52c
Peter Jönsson <peter.jonsson@klarna.com>
2013-11-14 23:22:47 +0100

    Simplify final URL call generation by using url.Values.

--- builder/cloudstack/api.go
+++ builder/cloudstack/api.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"sort"
 	"strings"
 )
 
@@ -54,90 +53,89 @@ func (cloudstack CloudStackClient) New(apiurl string, apikey string, secret stri
 
 // Create a SSH key
 func (c CloudStackClient) CreateKey(name string, pub string) (uint, error) {
-	NewRequest(c, "createSSHKeyPair")
+	params := url.Values{}
+	NewRequest(c, "createSSHKeyPair", params)
 	return 0, nil
 }
 
 // Deletes an SSH key
 func (c CloudStackClient) DeletesKey(id uint) error {
-	NewRequest(c, "deleteSSHKeyPair")
+	params := url.Values{}
+	NewRequest(c, "deleteSSHKeyPair", params)
 	return nil
 }
 
 // Deploys a Virtual Machine and returns it's id
-func (c CloudStackClient) DeployVirtualMachine(name string, size uint, image uint, region uint, keyId uint) (uint, error) {
-	NewRequest(c, "deployVirtualMachine")
+func (c CloudStackClient) DeployVirtualMachine(serviceofferingid string, templateid string, zoneid string) (uint, error) {
+	params := url.Values{}
+	params.Set("serviceofferingid", serviceofferingid)
+	params.Set("templateid", templateid)
+	params.Set("zoneid", zoneid)
+
+	NewRequest(c, "deployVirtualMachine", params)
 	return 0, nil
 }
 
 // Destroys a Virtual Machine
 func (c CloudStackClient) DestroyVM(id uint) error {
-	NewRequest(c, "destroyVirtualMachine")
+	params := url.Values{}
+	NewRequest(c, "destroyVirtualMachine", params)
 	return nil
 }
 
 // Powers off a Virtual Machine
 func (c CloudStackClient) StopVirtualMachine(id uint) error {
-	NewRequest(c, "stopVirtualMachine")
+	params := url.Values{}
+	NewRequest(c, "stopVirtualMachine", params)
 	return nil
 }
 
 // Shutdown a Virtual Machine
 func (c CloudStackClient) ShutdownVM(id uint) error {
-	NewRequest(c, "stopVirtualMachine")
+	params := url.Values{}
+	NewRequest(c, "stopVirtualMachine", params)
 	return nil
 }
 
 // Creates a snaphot of a Virtual Machine by it's ID
 func (c CloudStackClient) CreateSnapshot(id uint, name string) error {
-	NewRequest(c, "createSnapshot")
+	params := url.Values{}
+	NewRequest(c, "createSnapshot", params)
 	return nil
 }
 
 // Returns all available templates
 func (c CloudStackClient) Templates() ([]Template, error) {
-	NewRequest(c, "listTemplates")
+	params := url.Values{}
+	NewRequest(c, "listTemplates", params)
 	return nil, nil
 }
 
 // Deletes an template by its ID.
 func (c CloudStackClient) DeleteTemplate(id uint) error {
-	NewRequest(c, "deleteTemplate")
+	params := url.Values{}
+	NewRequest(c, "deleteTemplate", params)
 	return nil
 }
 
 // Returns CloudStack string representation of status "off" "new" "active" etc.
 func (c CloudStackClient) VMStatus(id uint) (string, string, error) {
-	NewRequest(c, "poweroff")
+	params := url.Values{}
+	NewRequest(c, "poweroff", params)
 	return "", "", nil
 }
 
-func NewRequest(c CloudStackClient, request string) {
-	args := make(map[string]string)
-	args["apikey"] = c.APIKey
-	args["command"] = request
-	args["response"] = "json"
-
-	// we need to create the URL with a list of (key, value) of
-	// arguments sorted in alphabetical order.
-	var keys []string
-	for k, _ := range args {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
-	// Create the initial api call string.
-	var params []string
-	for _, k := range keys {
-		params = append(params, (k + "=" + url.QueryEscape(args[k])))
-	}
-
-	s := strings.Join(params, "&")
+func NewRequest(c CloudStackClient, request string, params url.Values) {
+	params.Set("apikey", c.APIKey)
+	params.Set("command", request)
+	params.Set("response", "json")
 
 	// Generate signature for API call
+	// * Serialize parameters and sort them by key, done by Encode
 	// * Convert the entire argument string to lowercase
 	// * Calculate HMAC SHA1 of argument string with CloudStack secret
 	// * URL encode the string and convert to base64
+	s := params.Encode()
 	s2 := strings.ToLower(s)
 	mac := hmac.New(sha1.New, []byte(c.Secret))
 	mac.Write([]byte(s2))
